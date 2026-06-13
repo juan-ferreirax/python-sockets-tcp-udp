b48a6a7ec5a3ccaeac602dc1357c71e028e2d2ac
Juan Pablo <juanpablofc8.0@gmail.com>
2026-06-13 01:14:47 -0300

    feat: Adiciona versão base do cliente e do servidor

--- exerc1/cliente.py
+++ exerc1/cliente.py
@@ -1,1 +1,19 @@
 # Discentes: Juan Pablo Ferreira Costa, Nadson Nascimento Santos e Vitor Mozer Vieira Sales
+
+import socket
+
+ENDERECO_IP = "127.0.0.1"
+PORTA = 8000
+
+def start_client_tcp():
+    # Cria o socket TCP e realiza a conexão com o servidor
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ENDERECO_IP, PORTA))
+    print("Conexão estabelecida com sucesso!")
+
+    mensagem = "Mensagem do cliente"
+    client_socket.sendall(mensagem.encode("utf-8"))
+    client_socket.close()
+
+# Inicia o cliente TCP
+start_client_tcp()
--- exerc1/server.py
+++ exerc1/server.py
@@ -1,1 +1,23 @@
 # Discentes: Juan Pablo Ferreira Costa, Nadson Nascimento Santos e Vitor Mozer Vieira Sales
+
+import socket
+
+ENDERECO_IP = "127.0.0.1"
+PORTA = 8000
+
+def start_servidor_tcp():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cria o socket TCP
+    server_socket.bind((ENDERECO_IP, PORTA)) # Faz associação de IP e Porta
+    server_socket.listen()
+    print(f"Servidor ouvindo na porta {PORTA}...")
+
+    connection, address = server_socket.accept()
+    print("Conexão estabelecida com sucesso! Aguardando mensagem...")
+
+    data = connection.recv(1024).decode("utf-8")
+    print(data)
+
+    server_socket.close()
+
+# Inicia o servidor TCP
+start_servidor_tcp()
